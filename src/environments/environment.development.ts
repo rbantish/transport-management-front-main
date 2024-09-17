93b5c0ff24dfc13aeb1c5440df81dc60d626802e
rbantish <bantishramburn202100603@gmail.com>
2024-09-17 15:26:25 +0400

    update env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
-    apiUrl:"https://transport-management-back.onrender.com/api",
-    serverUrl: "https://transport-management-back.onrender.com",
-    env: "prod"
+    serverApiUrl:"http://localhost:30001/api",
+    serverUrl: "http://localhost:30001",
+    apiUrl: "http://localhost:40200/",
+    env: "local"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-    apiUrl:"https://transport-management-back.onrender.com/api",
+    serverApiUrl:"https://transport-management-back.onrender.com/api",
     serverUrl: "https://transport-management-back.onrender.com",
+    apiUrl: "https://transport-management-front.onrender.com",
     env: "prod"
 };
