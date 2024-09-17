86f8df9ef0ef98389b7524284b7517301cfb8728
rbantish <bantishramburn202100603@gmail.com>
2024-09-18 00:58:01 +0400

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    serverApiUrl:"http://localhost:30001/api",
-    serverUrl: "http://localhost:30001",
-    apiUrl: "http://localhost:40200/",
-    env: "local"
+    serverApiUrl:"https://transport-management-back.onrender.com/api",
+    serverUrl: "https://transport-management-back.onrender.com",
+    apiUrl: "https://transport-management-front.onrender.com",
+    env: "prod"
 };
