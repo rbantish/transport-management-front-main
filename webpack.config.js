d1cb7ef10edb5675d66afb61efbd7eb9fe193c90
rbantish <bantishramburn202100603@gmail.com>
2024-09-17 12:14:47 +0400

    update angular.json and webpack.config.ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,11 @@
-const path = require('path');
-
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
-};
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          loader: 'ignore-loader',
+        },
+      ],
+    },
+  };
+  
